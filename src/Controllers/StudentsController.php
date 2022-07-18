fe83c27d898a27a49e1ca23ae20412a639cc9814
Susana M.A <asturias.susi.pro@gmail.com>
2022-07-18 15:26:44 +0200

    delete implementation of the crud

--- src/Controllers/StudentsController.php
+++ src/Controllers/StudentsController.php
@@ -9,14 +9,26 @@ class StudentsController {
 
     public function __construct()
     {
+        if (isset($_GET["action"]) && ($_GET["action"] == "delete")) {
+
+            $this->delete($_GET["id"]);
+            return;
+        }
+        
         $this->index();
     }
-
 
     public function index() {
         $student = new Students();
         $students = $student->all();
 
         new View ("studentsHome", ["studentData" => $students]);
+    }
+
+    public function delete($id) {
+        $studentClassDone = new Students();
+        $studentDeleted = $studentClassDone->findByStudentId($id);
+        $studentDeleted->delete();
+        $this->index();
     }
 }
