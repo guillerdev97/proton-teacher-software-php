68809b633690b877523f1b31e54cf7328bec5edb
ozknkrm <kerimozkan11@gmail.com>
2022-07-19 15:31:44 +0200

    starting to style table

--- src/Views/studentsHome.php
+++ src/Views/studentsHome.php
@@ -7,29 +7,67 @@ require_once("components/layout.php")
     <?php
     require_once("components/header.php")
     ?>
-
-    <?php
-    foreach ($data["studentData"] as $student) {
-        echo "
-        <div class='container'>
-        <table class='table'>
-        <tr>
-            <td>{$student->getName()}</td>
-            <td>{$student->getClass()}</td>
-            <td>{$student->getDateTime()}</td>
-            <td>
-                <a href='?action=delete&id={$student->getId()}'>Eliminar</a>
-            </td>
-        </tr><br>
-        </table>
+    <main>
+        <div class="container-fluid">
+            <table class="table table-bordered overflow-auto">
+                <thead class="bg-dark text-light text-center">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Class</th>
+                        <th scope="col">Date Time</th>
+                        <th scope="col" class="lessWidth"></th>
+                    </tr>
+                </thead>
+                <tbody class="bg-light text-center">
+                    <?php
+                    foreach ($data["studentData"] as $student) {
+                        echo "
+                        <tr>
+                            <td>{$student->getName()}</td>
+                            <td>{$student->getClass()}</td>
+                            <td>{$student->getDateTime()}</td>
+                            <td>
+                                <a href='?action=delete&id={$student->getId()}'>Eliminar</a>
+                                <a href='#'>Edit</a>
+                            </td>
+                        </tr>
+                        ";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
-        ";
-    }
-    ?>
-
+    </main>
     <?php
     require_once("components/footer.php")
     ?>
 </body>
 
 </html>
+
+<style scoped>
+    .container-fluid{
+        margin:30px auto;
+        
+    }
+    .table{
+        width: 80%;
+        margin: auto;
+        overflow: scroll;
+    }
+    .lessWidth{
+        width: 10%;
+    }
+    td{
+     width: 30%;
+    }
+    thead{
+        position: sticky;
+    }
+    tbody{
+        max-height: 200px;
+      
+    }
+    
+
+</style>
