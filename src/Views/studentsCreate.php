e438c2d5bb1467cf46bc3eedac0bb115862f65e0
sudoBuda <juanfranciscobalseca@gmail.com>
2022-07-20 20:58:50 +0200

    form and stlyes of CREATE page

--- src/Views/studentsCreate.php
+++ src/Views/studentsCreate.php
@@ -12,27 +12,40 @@ require_once("components/layout.php")
         <form action='?action=store' method="post">
             <div class="container cardForm">
                 <div class="row">
-                    <div class="col-1"></div>
-                    <div class="col-10">
-                        <div class="col-4"><label for="name">Student</label></div>
-                        <div class="col-6"><input id="name" type="text" name="name" required placeholder="Student"></div>
+                    <div class="col-auto">
+                        <div class="row">
+                            <div class="col-2 nameLabel"><label for="name">NAME</label></div>
+                            <div class="col-8"><input id="name" type="text" name="name" required placeholder=" Student"></div>
+                        </div>
                     </div>
-                    <div class="col-1"></div>
                 </div>
 
                 <div class="row">
-                    <div class="col-1"></div>
-                    <div class="col-10">
-                        <div class="col-4"><label for="class">Class</label></div>
-                        <div class="col-6"><textarea id="class" type="text" name="class" required placeholder="Class"></textarea></div>
+                    <div class="col-auto">
+                        <div class="row">
+                            <div class="col-2"><label for="class">CLASS</label></div>
+                            <div class="col-8"><input id="class" type="text" name="class" required placeholder=" Class"></input></div>
+                        </div>
                     </div>
-                    <div class="col-1"></div>
                 </div>
-                <input type="submit" value="Create">
-                <input type="reset" value="Reset">
-                <a href="?action=backHome">
-                    <button type="button">Cancel</button>
-                </a>
+            </div>
+            <div class="container buttonsStack">
+                <div class="row">
+                    <div class="col-auto">
+                        <input type="submit" value="CREATE">
+                    </div>
+                    <div class="col-auto">
+                        <input type="reset" value="RESET">
+                    </div>
+                    <div class="col-auto">
+                        <a href="?action=backHome">
+                            <button class="buttonCancel" type="button">CANCEL</button>
+                        </a>
+                    </div>
+
+
+
+                </div>
             </div>
         </form>
     </main>
